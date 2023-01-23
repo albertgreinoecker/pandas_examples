5de1f7d5ad93749467f851d0b7e120d62ee47ea7
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2023-01-23 15:50:25 +0100

    visualization

--- ex_09_mann_whitney_u.py
+++ ex_09_mann_whitney_u.py
@@ -6,10 +6,10 @@ import seaborn as sns
 # Einlesen des Datensatzes
 df = pd.read_csv('data/student-mat.csv', sep=";")
 
-print(df.describe())
-print(df.columns)
+#print(df.describe())
+#print(df.columns)
 
-a = df['absences']
+a = df['Walc']
 a_m = a.loc[df['sex'] == 'M']
 a_f = a.loc[df['sex'] == 'F']
 
@@ -21,3 +21,9 @@ s, p = mannwhitneyu(a_m, a_f)
 
 print("test statistics:", s)
 print("p-value", p)
+
+print([a_f, a_m])
+
+x = pd.crosstab(df['Walc'],df['sex'], normalize='index')
+x.plot.bar(rot=0)
+plt.show()
