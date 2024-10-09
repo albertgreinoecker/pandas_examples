d2d2bba518b265961e27e420d5c2d9b73776a368
albert <albert.greinoecker@gmail.com>
2024-10-09 22:46:04 +0200

    add new examples

--- ex_03_pillow.py
+++ ex_03_pillow.py
@@ -1,35 +1,31 @@
 from PIL import Image
+from numpy import asarray
 
-# img = Image.open('img/htl-logo.png')
-#
-# print(img.format) # Speicherformat
-# print(img.size) # Dimensionen
-# print(img.mode) # Farbmodus
-# img.show() # Anzeigen des Bildes im default-Bildeditor
+img = Image.open('img/htl-logo.png')
+
+print(img.format) # Speicherformat
+print(img.size) # Dimensionen
+print(img.mode) # Farbmodus
+img.show() # Anzeigen des Bildes im default-Bildeditor
 
 
 #Laden des Bildes mit Matplotlib
-#
-# from matplotlib import image
-# from matplotlib import pyplot
-# image = image.imread('img/htl-logo.png')
-# print(image.dtype) # Werte als float32 gespeichert
-# print(image.shape) # (x,y, farbebenen) farb-ebenen = [r,g,b,opacity]
-# print(image[0][0]) # Pixel links oben
-# pyplot.imshow(image) # Bild auf die Zeichenfläche geben
-# pyplot.show()
+from matplotlib import image
+from matplotlib import pyplot
+image = image.imread('img/htl-logo.png')
+print(image.dtype) # Werte als float32 gespeichert
+print(image.shape) # (x,y, farbebenen) farb-ebenen = [r,g,b,opacity]
+print(image[0][0]) # Pixel links oben
+pyplot.imshow(image) # Bild auf die Zeichenfläche geben
+pyplot.show()
 
 
-#Laden des Bildes als numpy-Array
-
-from PIL import Image
-from numpy import asarray
-image = Image.open('img/htl-logo.png')
 # Übertragen des Bildes in ein numpy-Array
-data = asarray(image)
+data = asarray(img)
 print(data[0][2]) #Pixel links oben
 # Hier könnte die Manipulation stattfinden
 print(data.shape) #Dimensionen des Bildes (x,y, farb-ebenen)
 # So kann man aus dem numpy-array wieder ein PIL-Bild machen
 image2 = Image.fromarray(data)
+
 
--- ex_03_pillow_image_manipulation.py
+++ ex_03_pillow_image_manipulation.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 # Als Graystufenbild speichern
 #Weitere Modi: "1", 'L', 'P', 'RGB', 'RGBA', 'CMYK', 'YCbCr', 'HSV', "I", 'F'
-# im = np.array(Image.open('img/htl-logo.png').convert('L'))
-# #ist jetzt ein numpy-Array
-# print(im.shape) #(x,y)
-# print(im[0][0])
-# gr_im= Image.fromarray(im).save('img/htl-logo-gray.png')
+im = np.array(Image.open('img/htl-logo.png').convert('L'))
+#ist jetzt ein numpy-Array
+print(im.shape) #(x,y)
+print(im[0][0])
+gr_im= Image.fromarray(im).save('img/htl-logo-gray.png')
 
 
 #größe Ändern
@@ -19,3 +19,44 @@ Image.fromarray(img_small).save('img/htl-logo-small.png')
 im = np.array(Image.open('img/htl-logo.png'))
 im_trim = im[150:300, 330:1350]
 Image.fromarray(im_trim).save('img/htl-logo-trim.png')
+
+## Weitere Effekte
+
+# Graustufen selbst berechnen
+im_gray = np.mean(im, axis=2).astype(np.uint8)
+Image.fromarray(im_gray).save('img/htl-logo-gray2.png')
+
+# Schwarz-Weiß
+im_gray[im_gray < 128] = 0
+im_gray[im_gray >= 128] = 255
+Image.fromarray(im_gray).save('img/htl-logo-bw.png')
+
+# bild spiegeln
+im_flip = np.fliplr(im)
+Image.fromarray(im_flip).save('img/htl-logo-flip.png')
+
+# Rot-Kanal abdrehen:
+im_red = im.copy()
+im_red[:,:,0] = 0
+Image.fromarray(im_red).save('img/htl-logo-no_red.png')
+
+# Rotes Gitter
+im_raster = im.copy()
+im_raster[ ::10,:,0] = 255
+im_raster[ :,::10,0] = 255
+Image.fromarray(im_raster).save('img/htl-logo-raster.png')
+
+
+pos_red = np.where(im[:,:,0] > 128)
+im_pos_red = im.copy()
+print(im_pos_red.shape)
+print(pos_red)
+im_pos_red[pos_red[0], pos_red[1], :3] = [255,0, 0] #RGBA, alpha bleibt unverändert
+print(im_pos_red.shape)
+Image.fromarray(im_pos_red).save('img/htl-logo-pos_red.png')
+
+# Schwarzes Rechteck im Zentrum
+im_black_rect = im.copy()
+im_black_rect[85:180, 85:180] = [0,0,0,255]  #RGBA
+Image.fromarray(im_black_rect).save('img/htl-logo-black_rect.png')
+
