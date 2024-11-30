372dbb289ce7b2b0a37bc0a23ea53687f360c110
albert <albert.greinoecker@gmail.com>
2024-11-30 18:58:10 +0100

    different example

--- ex_07_pandas_chi_square.py
+++ ex_07_pandas_chi_square.py
@@ -3,7 +3,6 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy.stats import chi2_contingency
-import statsmodels.api as sm
 
 # Einlesen des Datensatzes
 df = pd.read_csv('data/student-mat.csv', sep=";")
@@ -11,29 +10,29 @@ df = pd.read_csv('data/student-mat.csv', sep=";")
 print(df.describe())
 print(df.columns)
 
-ct_sex_higher = pd.crosstab(df['sex'], df['higher'])
-print(ct_sex_higher)
+ct_internet_higher = pd.crosstab(df['internet'], df['higher'])
+print(ct_internet_higher)
 
-ct_sex_higher_pct = pd.crosstab(df['sex'], df['higher'], normalize='all')
-print(ct_sex_higher_pct)
+ct_internet_higher_pct = pd.crosstab(df['internet'], df['higher'], normalize='all')
+print(ct_internet_higher_pct)
 
-# sns.heatmap(ct_sex_higher, annot=True, cmap="YlGnBu", fmt='d')
-# plt.show()
+sns.heatmap(ct_internet_higher, annot=True, cmap="YlGnBu", fmt='d')
+plt.show()
 
-
-chi, p, dof, expected = chi2_contingency(ct_sex_higher)
+chi, p, dof, expected = chi2_contingency(ct_internet_higher)
 print ("Chi:",chi)
 print ("p:", p)
 print ("dof" ,dof)
 print ("expected",expected)
 
-sns.heatmap(ct_sex_higher, annot=False, cmap="YlGnBu")
-sns.heatmap(ct_sex_higher, annot=ct_sex_higher, annot_kws={'va':'bottom'}, fmt="", cbar=False , cmap="YlGnBu")
-sns.heatmap(ct_sex_higher, annot=expected, annot_kws={'va':'top'}, fmt=".2f", cbar=False, cmap="YlGnBu")
+sns.heatmap(ct_internet_higher, annot=False, cmap="YlGnBu")
+
+sns.heatmap(ct_internet_higher, annot=ct_internet_higher, annot_kws={'va':'bottom'}, fmt="", cbar=False , cmap="YlGnBu")
+sns.heatmap(ct_internet_higher, annot=expected, annot_kws={'va':'top'}, fmt=".2f", cbar=False, cmap="YlGnBu")
 plt.show()
 
-print (type(ct_sex_higher), type(expected))
+
 ex = pd.DataFrame(expected)
 print(ex)
-both = pd.concat([ct_sex_higher, ex])
-print(both)
+
+
